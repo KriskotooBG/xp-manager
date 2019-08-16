994b19b76f820071688bf7332a7ed13af9ba221c
KriskotooBG <kristianilianov630@gmail.com>
2019-08-17 00:24:14 +0300

    Created Main class base

---
+++ src/kriskotooBG/XPMaster/Main.php
@@ -0,0 +1,19 @@
+<?php
+	namespace kriskotooBG\XPMaster;
+	
+	use pocketmine\plugin\PluginBase;
+	use pocketmine\command\Command;
+	use pocketmine\command\CommandSender;
+	
+	
+	
+	class Main extends PluginBase{
+		
+		
+		
+		
+		
+		
+		
+	}
+?>
